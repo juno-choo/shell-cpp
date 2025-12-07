f00db62a7b593375365d35f3b5957bc8b1675090
Juno Choo <choojunhoe@gmail.com>
2025-12-07 10:51:03 -0600

    Handle invalid commands

--- src/main.cpp
+++ src/main.cpp
@@ -9,4 +9,10 @@ int main() {
 
   // TODO: Uncomment the code below to pass the first stage
   cout << "$ ";
+
+  string command;
+  getline(cin, command);
+  cout << command << ": command not found " << endl;
+
+  return 0;
 }
