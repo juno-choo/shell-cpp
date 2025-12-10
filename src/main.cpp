f15fa864d0e2f5a18fd079643a5e0548ad9c2ffe
Juno Choo <choojunhoe@gmail.com>
2025-12-09 23:58:42 -0600

    type

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
+const vector<string> BUILTINS = {"echo", "exit", "cd", "ls", "pwd"};
+
+bool isBuiltin(const string& cmd) {
+  for (const auto& builtin : BUILTINS) {
+    if (cmd == builtin) {
+      return true;
+    }
+  }
+  return false;
+}
 int main() {
   // Flush after every std::cout / std:cerr
   cout << unitbuf;
   cerr << unitbuf;
-
+  
   while (true) {
     cout << "$ ";
 
@@ -27,9 +38,17 @@ int main() {
     else if (command == "echo") {
       cout << line << endl;
     }
+    else if (command == "type") {
+      if (isBuiltin(line)) {
+        cout << line << " is a shell builtin" << endl;
+      } 
+      else {
+        cout << line << " not found" << endl;
+      }
+    }
     else {
       cout << command << ": command not found " << endl;  
     } 
-  }
+  }   
   return 0;
 }
